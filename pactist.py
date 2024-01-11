a54b9de18e4e987871d370ebc885425c9f44500a
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2024-01-11 09:42:55 +0200

    Add files via upload

--- pactist.py
+++ pactist.py
@@ -1,9 +1,17 @@
-secret_word = input("enter the secret word")
+secret_word = input("enter the secret word: ")
+guess_limit = int(input("enter the guess limit: "))
+print("\n\n\n\n\n\n")
 print("pass over the game to you friend")
-
 guess = ""
 while guess != secret_word:
+    num_of_guesses += 1
+    if(num_of_guesses == guess_limit):
+        print("well i guess you lose")
+        break
+    
     if (guess != ""):
         print("try again")
-        guess = input("enter the guess")
+        guess = input("enter the guess: ")
+
+    if(guess == secret_word):
         print("CONGRATULATIONS YOU WIN")
